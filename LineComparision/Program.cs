1a9d2214968a0e7daf0ea5d1a573c79f3e4090d2
karanmahi71 <karankasbe7171@gmail.com>
2022-08-24 23:04:27 +0530

    Check equality of two lines

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Comparision comparision = new Comparision(1, 2, 3, 4);
+            Comparision comparision = new Comparision(1,2,3,4);
+            double line1 = comparision.CalculateLength();
+            Console.WriteLine("Length of first line"+line1);
+            comparision LineComparision = new comparision(1, 2, 3, 4);
+            double line1 = comparision.CalculateLength();
+            Console.WriteLine("Length of first line"+line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("Line 1 and Line 2 are equal");
+            }
+            else
+                Console.WriteLine("Not equal");
         }
     }
 }
