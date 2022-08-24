ab151a8446a143702efe7360673ebc7399aad61a
karanmahi71 <karankasbe7171@gmail.com>
2022-08-24 22:40:04 +0530

    Calculate its lenghth

---
+++ LineComparision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparision comparision = new Comparision(1, 2, 3, 4);
+        }
+    }
+}
