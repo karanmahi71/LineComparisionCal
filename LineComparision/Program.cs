7e10e0537eea46154f1576421a3b543bf572438f
karanmahi71 <karankasbe7171@gmail.com>
2022-08-24 23:16:18 +0530

    greter thn less than

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,12 +10,15 @@
             comparision LineComparision = new comparision(1, 2, 3, 4);
             double line1 = comparision.CalculateLength();
             Console.WriteLine("Length of first line"+line2);
-            if (line1 == line2)
+            if (line1.CompareTo(line2) == 0)
             {
-                Console.WriteLine("Line 1 and Line 2 are equal");
+                Console.WriteLine("Both line are same");
+                return;
             }
+            if (line1.CompareTo(line2) > 0)
+                Console.WriteLine("Line 1 is greter than Line 2");
             else
-                Console.WriteLine("Not equal");
+                Console.WriteLine("Line 2 is greter than Line 1");
         }
     }
 }
